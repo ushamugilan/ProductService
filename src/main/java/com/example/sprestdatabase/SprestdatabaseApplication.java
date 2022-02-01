bc2bc1ab738683a50676e56b714bf88de15dd200
Usha Nandhini <ushadsp@gmail.com>
2022-01-31 19:42:51 -0600

    1

--- src/main/java/com/example/sprestdatabase/SprestdatabaseApplication.java
+++ src/main/java/com/example/sprestdatabase/SprestdatabaseApplication.java
@@ -3,7 +3,7 @@ package com.example.sprestdatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication //commit 1/31
+@SpringBootApplication //commit 1/32
 public class SprestdatabaseApplication {
 
 	public static void main(String[] args) {
