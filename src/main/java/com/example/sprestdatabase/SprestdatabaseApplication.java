752a1d4c53edeeb08edf4dffc7f92b651ffb9ec0
Usha Nandhini <ushadsp@gmail.com>
2022-02-02 17:07:15 -0600

    git

--- src/main/java/com/example/sprestdatabase/SprestdatabaseApplication.java
+++ src/main/java/com/example/sprestdatabase/SprestdatabaseApplication.java
@@ -3,7 +3,7 @@ package com.example.sprestdatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication //commit 2/5
+@SpringBootApplication //git repo->remotes->origin->configure push->change->username and token from git
 public class SprestdatabaseApplication {
 
 	public static void main(String[] args) {
